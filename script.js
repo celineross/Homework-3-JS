5e502915b9be3cbe7e0789d5e339ed32c64e4d1b
Celine Ross <celineross@gmail.com>
2020-08-06 18:31:40 -0500

    some pseudocode written

---
+++ script.js
@@ -0,0 +1,16 @@
+function generate () {
+
+}
+
+
+
+
+/* click generate pw
+prompt length of the password
+confirm prompt option for lowercase
+confirm prompt option for uppercase
+confirm prompt option for numbers
+confirm prompt option for special characters
+
+i
+*/
