40582effba649497273fe4930cbcdfe2f5afe61e
Celine Ross <celineross@gmail.com>
2020-08-07 11:33:15 -0500

    completed first prompt

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-var lowercase = "a b c d e f g h i j k l m n o p q r s t u v w z y z";
-var uppercase = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
-var numbers = "0 1 2 3 4 5 6 7 8 9 0";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
 var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 var generateButton = document.getElementById("generate");
 
 
 generateButton.addEventListener("click", function() {
-    var pLength = prompt("How many characters would you like?")
-
+    var pLength = prompt("How many characters would you like?"); 
+    if (pLength < 8 || pLength > 128) {
+        alert("Please enter a value between 8 and 128 characters.")
+    } else {
+        alert("You have chosen " + pLength + " characters. Press OK to continue.")
+    }
 })
 
 /* click generate pw
 prompt length of the password
+    ask for length
+    make sure its between 8 and 128 char
+    reject if less than 8 or more than 128
 confirm prompt option for lowercase
 confirm prompt option for uppercase
 confirm prompt option for numbers
