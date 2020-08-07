f21b0513f04cea58b57e6ba6c5775df068b396d4
Celine Ross <celineross@gmail.com>
2020-08-07 11:14:05 -0500

    prompt for pw length

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function generate () {
+var lowercase = "a b c d e f g h i j k l m n o p q r s t u v w z y z";
+var uppercase = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+var numbers = "0 1 2 3 4 5 6 7 8 9 0";
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-}
+var generateButton = document.getElementById("generate");
 
 
+generateButton.addEventListener("click", function() {
+    var pLength = prompt("How many characters would you like?")
 
+})
 
 /* click generate pw
 prompt length of the password
